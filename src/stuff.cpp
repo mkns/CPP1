9626c786590ba2f63a19954f8e9c4df30842256e
Kenny Scott <kenny@kennyscott.co.uk>
2011-03-20 15:05:07 +0000

    vectors

--- src/stuff.cpp
+++ src/stuff.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 using namespace std;
 
@@ -13,10 +14,12 @@ int intro_to_typecasting();
 int intro_to_classes();
 void handle_command_line_arguments(int argc, char *argv[]);
 void generate_linked_list();
+int intro_to_vectors();
 
 int main(int argc, char *argv[]) {
 	handle_command_line_arguments(argc, argv);
 	generate_linked_list();
+	intro_to_vectors();
 	return intro_to_classes();
 }
 
@@ -244,4 +247,31 @@ void generate_linked_list() {
 		nextElement = nextElement->next;
 	}
 
+}
+
+int intro_to_vectors() {
+	vector<int> example; //Vector to store integers
+	example.push_back(3); //Add 3 onto the vector
+	example.push_back(10); //Add 10 to the end
+	example.push_back(33); //Add 33 to the end
+	for (unsigned int x = 0; x < example.size(); x++) {
+		cout << example[x] << " "; //Should output: 3 10 33
+	}
+	cout << endl;
+
+	if (!example.empty()) //Checks if empty
+		example.clear(); //Clears vector
+
+	vector<int> another_vector; //Creates another vector to store integers
+	another_vector.push_back(10); //Adds to end of vector
+	example.push_back(10); //Same
+	if (example == another_vector) //To show testing equality
+	{
+		example.push_back(20);
+	}
+	for (unsigned int y = 0; y < example.size(); y++) {
+		cout << example[y] << " "; //Should output 10 20
+	}
+	cout << endl;
+	return 0;
 }
